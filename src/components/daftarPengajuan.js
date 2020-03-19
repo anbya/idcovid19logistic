9f5b27f6623f3ffbcc2517c07ae997cb8c12bcb9
anbya <anbyaalibia@gmail.com>
2020-03-19 11:19:48 +0700

    190320201119

--- src/components/daftarPengajuan.js
+++ src/components/daftarPengajuan.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios'
 import {
   Card,
   CardBody,
@@ -10,27 +11,67 @@ import {
 } from "reactstrap";
 
 class daftarPengajuan extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataPengajuan:[],
+      placeholder: "show"
+    };
+  }
+  componentDidMount = () => {
+    axios.get(`http://localhost:8080/idCovid19/cekpengajuan`)
+    .then( async result1 => {
+      if (result1.data.status == "00") {
+        await this.setState({
+          ...this.state,
+          dataPengajuan: result1.data.result,
+          placeholder: "none"
+        });
+      } else {
+        await this.setState({
+          ...this.state,
+          placeholder: "none"
+        });
+      }
+    })
+    .catch(error1 => {
+      console.log(error1);
+    });
+  }
   render() {
-    return (
-      <Card style={{ width: "100%" }}>
-        <CardHeader>
-          <h1>Daftar pengajuan</h1>
-        </CardHeader>
-        <CardBody style={{padding:"0px",minHeight:"50vh",overflowX:"hidden",overflowY:"scroll"}}>
-          <ListGroup>
-            <ListGroupItem>
-              <ListGroupItemHeading>
-                List group item heading
-              </ListGroupItemHeading>
-              <ListGroupItemText>
-                Donec id elit non mi porta gravida at eget metus. Maecenas sed
-                diam eget risus varius blandit.
-              </ListGroupItemText>
-            </ListGroupItem>
-          </ListGroup>
-        </CardBody>
-      </Card>
-    );
+    if(this.state.dataPengajuan.length > 0){
+      return (
+        <Card style={{ width: "100%" }}>
+          <CardHeader>
+            <h1>Daftar pengajuan</h1>
+          </CardHeader>
+          <CardBody style={{padding:"0px",minHeight:"50vh",overflowX:"hidden",overflowY:"scroll"}}>
+            <ListGroup>
+              <ListGroupItem>
+                <ListGroupItemHeading>
+                  List group item heading
+                </ListGroupItemHeading>
+                <ListGroupItemText>
+                  Donec id elit non mi porta gravida at eget metus. Maecenas sed
+                  diam eget risus varius blandit.
+                </ListGroupItemText>
+              </ListGroupItem>
+            </ListGroup>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return (
+        <Card style={{ width: "100%" }}>
+          <CardHeader>
+            <h1>Daftar pengajuan</h1>
+          </CardHeader>
+          <CardBody style={{padding:"0px",minHeight:"50vh",overflowX:"hidden",overflowY:"scroll"}}>
+            <h1>Daftar pengajuan kosong</h1>
+          </CardBody>
+        </Card>
+      );
+    }
   }
 }
 
--- src/components/daftarProsespengajuan.js
+++ src/components/daftarProsespengajuan.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios'
 import {
   Card,
   CardBody,
@@ -10,27 +11,67 @@ import {
 } from "reactstrap";
 
 class daftarProsespengajuan extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataPengiriman:[],
+      placeholder: "show"
+    };
+  }
+  componentDidMount = () => {
+    axios.get(`http://localhost:8080/idCovid19/cekpengiriman`)
+    .then( async result1 => {
+      if (result1.data.status == "00") {
+        await this.setState({
+          ...this.state,
+          dataPengiriman: result1.data.result,
+          placeholder: "none"
+        });
+      } else {
+        await this.setState({
+          ...this.state,
+          placeholder: "none"
+        });
+      }
+    })
+    .catch(error1 => {
+      console.log(error1);
+    });
+  }
   render() {
-    return (
-      <Card style={{ width: "100%" }}>
-        <CardHeader>
-          <h1>Daftar pengiriman</h1>
-        </CardHeader>
-        <CardBody style={{padding:"0px",minHeight:"50vh",overflowX:"hidden",overflowY:"scroll"}}>
-          <ListGroup>
-            <ListGroupItem>
-              <ListGroupItemHeading>
-                List group item heading
-              </ListGroupItemHeading>
-              <ListGroupItemText>
-                Donec id elit non mi porta gravida at eget metus. Maecenas sed
-                diam eget risus varius blandit.
-              </ListGroupItemText>
-            </ListGroupItem>
-          </ListGroup>
-        </CardBody>
-      </Card>
-    );
+    if(this.state.dataPengiriman.length > 0){
+      return (
+        <Card style={{ width: "100%" }}>
+          <CardHeader>
+            <h1>Daftar pengiriman</h1>
+          </CardHeader>
+          <CardBody style={{padding:"0px",minHeight:"50vh",overflowX:"hidden",overflowY:"scroll"}}>
+            <ListGroup>
+              <ListGroupItem>
+                <ListGroupItemHeading>
+                  List group item heading
+                </ListGroupItemHeading>
+                <ListGroupItemText>
+                  Donec id elit non mi porta gravida at eget metus. Maecenas sed
+                  diam eget risus varius blandit.
+                </ListGroupItemText>
+              </ListGroupItem>
+            </ListGroup>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return (
+        <Card style={{ width: "100%" }}>
+          <CardHeader>
+            <h1>Daftar pengiriman</h1>
+          </CardHeader>
+          <CardBody style={{padding:"0px",minHeight:"50vh",overflowX:"hidden",overflowY:"scroll"}}>
+            <h1>Daftar pengiriman kosong</h1>
+          </CardBody>
+        </Card>
+      );
+    }
   }
 }
 
